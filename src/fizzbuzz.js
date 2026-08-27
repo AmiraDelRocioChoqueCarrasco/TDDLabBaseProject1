5924d2e83d16d20f058da451d058a5ed2eeeb3db
AMIRA DEL ROCÍO CHOQUE CARRASCO <amira.choque@ucb.edu.bo>
2026-08-27 10:05:33 -0400

    feat: Caso de prueba cuando el numero es 1

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,9 @@
-function fizzbuzz(){
-    return "2";
+function fizzbuzz(num) {
+    if (num == 1) {
+        return "1";
+    }else if (num % 3 != 0 & num % 5 != 0) {
+        return num.toString();
+    }
 }
+
 export default fizzbuzz;
--- src/fizzbuzz.test.ts
+++ src/fizzbuzz.test.ts
@@ -3,7 +3,11 @@ import fizzbuzz from "./fizzbuzz.js";
 
 describe("fizzBuzz", () => {
    it("debería generar el mismo numero si no sigue una regla", () => {
-     expect(fizzbuzz(2)).toBe("2");
+     expect(fizzbuzz(2)).toEqual("2");
+   });
+
+   it("debería generar el numero otro numero que no sigue una regla", () => {
+     expect(fizzbuzz(1)).toEqual("1");
    });
 });
 
